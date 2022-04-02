dc2f7bf0e84b61bd1b4cb0ef37dd81096808204e
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2022-04-02 17:12:38 +0900

    immer-tutorial

---
+++ 11/router-tutorial/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
+const App = () => {
+  return  (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
